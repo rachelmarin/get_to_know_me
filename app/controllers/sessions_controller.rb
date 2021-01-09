c71881b3863fe06f970fb0e5606120c9ce7b5f41
rachelmarin <Rachelmarinpal@gmail.com>
2021-01-08 22:20:59 -0500

    new sessions controller file

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,38 @@
+class SessionsController < ApplicationController
+        
+    get '/signup' do
+        redirect_if_logged_in
+      erb :'sessions/signup'
+    end
+  
+    post '/signup' do
+      user = User.new(params[:user])
+      if user.save
+        session[:user_id] = user.id
+        redirect "/signs"
+      else
+        redirect "/signup"
+      end
+    end
+  
+    get '/login' do
+      redirect_if_logged_in
+      erb :'sessions/login'
+    end
+  
+    post '/login' do
+      user = User.find_by_username(params[:username])
+     
+      if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        redirect "/signs"
+      else
+        redirect "/login"
+      end
+    end
+  
+    get '/logout' do
+      session.clear
+      redirect '/login'
+    end
+  end
