6069aaaac73aae4de8595e32de70fbdacaf2390f
rachelmarin <Rachelmarinpal@gmail.com>
2021-01-08 22:25:08 -0500

    updated tables working

---
+++ db/migrate/02_create_signs.rb
@@ -0,0 +1,14 @@
+class CreateSigns < ActiveRecord::Migration
+    def change
+      create_table :signs do |t|
+        t.integer :user_id
+        t.string :name
+        t.date :date_of_birth
+        t.string :z_s
+        t.string :c_z_s
+        t.string :birthstone
+
+        
+    end
+  end
+end
