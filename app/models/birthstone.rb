f7e85ebc98fc21bebf9c3b571819610887ca7019
rachelmarin <Rachelmarinpal@gmail.com>
2021-01-08 22:21:49 -0500

    cleared info in class

--- app/models/birthstone.rb
+++ app/models/birthstone.rb
@@ -1,3 +1,47 @@
 class Birthstone < ActiveRecord::Base
-    belongs_to 
-   end
+    # require 'date'
+end
+#     belongs_to :signs
+  
+
+#     attr_accessor :month
+
+#     @@all = []
+    
+#         def initialize(month)
+#             @month = month
+            
+#         end
+#     end
+
+#         def birth_month(stone)
+#             if month = "January"
+#                 puts "Garnet, Meaning: Protection"
+#             elsif month == "February"
+#                 puts "Amethyst, Meaning: Wisdom"
+#             elsif birth_month == "March"
+#                 puts "Aquamarine, Meaning: Serenity "
+#             elsif birth_month == "April"
+#                 puts "Diamond, Meaning: Strength"
+#             elsif birth_month == "May"
+#                 puts "Emerald, Meaning: Hope"
+#             elsif birth_month == "June"
+#                 puts "Pearl or Alexandrite, Meaning: Love"
+#             elsif birth_month == "July"
+#                 puts "Ruby, Meaning: Vitality"
+#             elsif birth_month == "August"
+#                 puts "Peridot, Meaning: Beauty"
+#             elsif birth_month == "September"
+#                 puts "Sapphire, Meaning: Truth"
+#             elsif birth_month == "October"
+#                 puts "Tourmaline, Meaning: Healing"
+#             elsif birth_month == "November"
+#                 puts "Citrine, Meaning: Joy"
+#             elsif birth_month == "December"
+#                 puts "Turquoise, Meaning: Friendship"
+#             else
+#                 puts "Please enter a birth month."
+#             end
+       
+    
+#    end
