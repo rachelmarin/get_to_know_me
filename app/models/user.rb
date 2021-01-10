04856c366d690cf8e72b76c903e029676534ee14
rachelmarin <Rachelmarinpal@gmail.com>
2021-01-10 05:21:29 -0500

    user model edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
     has_many :signs
+    has_many :birthstones, through: :signs
     has_secure_password
   
     validates :username, presence: true, uniqueness: true
